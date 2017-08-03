f1fd168df60a587314aa2bcf5910178f88726fd4
bobram22 <league@WorthyMini.local>
2017-08-02 18:31:20 -0700

    again

--- src/RunningOfTheRibs.java
+++ src/RunningOfTheRibs.java
@@ -1,4 +1,6 @@
 
 public class RunningOfTheRibs {
-
+public static void main(String[] args) {
+	ribs yum = new ribs(8,"pork");
+}
 }
--- src/ribs.java
+++ src/ribs.java
@@ -1,4 +1,31 @@
-
 public class ribs {
-
+private int numOfRibs;
+private String BorP;
+public ribs(int numOfRibs,String BorP){
+	this.numOfRibs=numOfRibs;
+	this.BorP=BorP;
+}
+public int getnumOfRibs(){
+	return this.numOfRibs;
+}
+public void setnumOfRibs(int numOfRibs){
+	if(numOfRibs<9){
+		this.numOfRibs=numOfRibs;
+	}
+	else{
+		this.numOfRibs=9;
+	}
+}
+public void mustbeBorP(){
+	if(BorP.equals("beef")){
+		BorP.equals("beef");
+	}
+	else if(BorP.equals("pork")){
+		BorP.equals("pork");
+	}
+	else{
+		System.out.println("That ain't a rib kind");
+	}
+	
+}
 }
